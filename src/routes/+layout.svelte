1c6cf98ef60d414e7804ebf0779effb0a55fb95d
axotellix <rsandrey@list.ru>
2025-05-05 01:39:29 +0400

    EDIT: css -> scss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+    import '$lib/styles/styles.scss';
+</script>
+
+<aside>
+    <h1>Menu</h1>
+    <ul>
+        <li><a href="/">home</a></li>
+        <li><a href="/1">1</a></li>
+        <li><a href="/2">2</a></li>
+        <li><a href="/3">3</a></li>
+    </ul>
+</aside>
+
+<slot />
---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { page } from '$app/stores'
+    const id = $derived($page.params.slug)
+</script>
+
+<h1>Page {id}</h1>
+<p>some description ...</p>
