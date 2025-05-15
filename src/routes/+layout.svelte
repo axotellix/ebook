8ca1736d1a12eaf574c6e5dfa6755b2e3b76feca
axotellix <rsandrey@list.ru>
2025-05-16 02:35:36 +0400

    Added > pagination (js) | Designed > btn component | EDIT: vars (added white-to-black tones), Sidebar.scss (refactored code)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,9 +10,28 @@
     
 
     // [ PRESETS ]
+     // chapter content 
     let { children } = $props()
+
+     // chapter pagination presets
     let chapter = $derived(page.url.pathname.slice(1))
+    let chapter_prev = $derived(+chapter - 1)
+    let chapter_next = $derived(+chapter + 1)
     const chapters_num = 11
+
+
+    // [ FUNCTIONS ] 
+    const prevBtnCaption = ( c ) => {
+        if( c == 0 )            return 'назад'          // [fallback]
+        if( c == 1 )            return 'введение'       // [0]
+        if( c > 1 && c <= 8 )   return `глава ${--c}`   // [1-...]
+        if( c > 8 )             return `пред. раздел`   // [1-...]
+    }
+    const nextBtnCaption = ( c ) => {
+        if( c < 7 )             return `глава ${++c}`    // [1-7]
+        if( c >= 7 )            return `сл. раздел`           // [8-...]
+    }
+
     
     // get > sidebar open state (passed from Sidebar.svelte)
     let sidebarOpen = $state(true)
@@ -35,8 +54,14 @@
         <!-- [ Pagination ] -->
         <nav class="chapter-switch">
             <hr />
-            <button class:btn-inactive={ chapter == 0 }><i class="icon-arrow-left"></i> prev</button>
-            <button class:btn-inactive={ chapter == chapters_num }>next <i class="icon-arrow-right"></i></button>
+            <a href={`/${chapter_prev}`} class="btn" class:btn-inactive={ chapter == 0 }>
+                <i class="icon-arrow-left"></i>
+                { prevBtnCaption(chapter) }
+            </a>
+            <a href={`/${chapter_next}`} class="btn" class:btn-inactive={ chapter == chapters_num }>
+                { nextBtnCaption(chapter) }
+                <i class="icon-arrow-right"></i>
+            </a>
         </nav>
 
     </main>
