a578e09925172201c62d48395cedd33b07b51b09
axotellix <rsandrey@list.ru>
2025-05-17 01:11:29 +0400

    Added > converter.js | Set > script to run conversion from root folder [package.json]

---
+++ src/lib/converter/converter.js
@@ -0,0 +1,96 @@
+
+// [---- IMPORTS ----]
+import mammoth from "mammoth";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+
+// [---- PRESETS ----]
+// get > current folder
+const __filename = fileURLToPath(import.meta.url);
+const currentFolder = path.dirname(__filename);
+
+
+// transform > table
+function transformTable(table) {
+
+	// check > if first row is bold (potential thead)
+	if (table.children[0].children[0].children[0].children[0].isBold) {
+		table.children[0].isHeader = true   // set > row as <thead>
+	}
+
+	return table
+
+}
+
+// pretransform > DOM
+function transformElement(element) {
+
+	if (element.children) {
+		const newChildren = element.children.map(transformElement)
+		element = { ...element, children: newChildren }
+	}
+
+	if (element.type === "table") {
+		element = transformTable(element)
+	}
+
+	return element
+
+}
+
+//@ split > html by H1
+const splitByH1 = ( h ) => {
+
+	// contain > chapters content as arr elements
+	let chapters = []
+
+	//? while > there are h1-s (above the 1st h1)
+	while (h.lastIndexOf("<h1>") > 0) {
+
+		// put > this block into array
+		chapters.unshift( h.slice(h.lastIndexOf("<h1>")) )
+
+		// slice > this block out of HTML 
+		h = h.slice(0, h.lastIndexOf("<h1>"))
+
+	}
+
+	// put > the 1st h1 as is
+	chapters.unshift(h)
+
+	// return > chapters HTML split into array
+	return chapters
+
+}
+
+// set > options for the conversion
+const options = {
+	styleMap: [
+		"p[style-name='Image Caption'] => p.piccaption:fresh",
+	],
+	transformDocument: transformElement
+}
+
+
+// [---- MAIN ----]
+// convert > docx to html
+mammoth.convertToHtml({ path: currentFolder + "/doc.docx" }, options)
+	.then(function (result) {
+		let chapters = []
+		let html = result.value
+
+		chapters = splitByH1(html)
+
+		// save > html
+		for( let i = 0; i < chapters.length; i++ ) {
+			fs.writeFile(currentFolder + `/output-${i}.html`, chapters[i], function (err) {
+				if (err) {
+					return console.log(err);
+				}
+				console.log(`The file output-${i}.html was created!`);
+			});
+		}
+
+	}).done();
